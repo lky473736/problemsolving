8f5b0f3c54c5c1aaa419e8810baa2757d5dceea3
임규연 <lky473736@icloud.com>
2023-10-12 11:07:08 +0000

    Create Q8.cpp

---
+++ solution for self-made/quiz for learningC/midterm/Q8.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char mode;
+    int binary;
+    
+    scanf ("%c%d", &mode, &binary);
+    
+    if (mode == 'O')
+    {
+        printf ("%o", binary);
+    }
+    
+    else if (mode == 'H')
+    {
+        printf ("%x", binary);
+    }
+}
