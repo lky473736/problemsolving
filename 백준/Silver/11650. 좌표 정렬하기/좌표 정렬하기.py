b119d1a252116cb8fb3b6ccf12a36fa0b3132ea8
임규연 <lky473736@icloud.com>
2023-11-07 01:26:41 +0900

    Update 좌표 정렬하기.py

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,3 +1,24 @@
+'''import sys
+
+N = int(sys.stdin.readline().rstrip())
+p = []
+
+for i in range (N) :
+    compo = list(map(int, input().split()))
+    p.append (compo)
+    
+p.sort(key = lambda x : x[0]) # x값 오름차순 정렬
+
+for j in range (N - 1) : # x값이 서로 같을 때 y값 오름차순 정렬
+    if p[j][0] == p[j + 1][0] :
+        if p[j][1] > p[j + 1][1] : 
+            p[j][1], p[j + 1][1] = p[j + 1][1], p[j][1]
+            
+for k in p :
+    x = k[0]
+    y = k[1]
+    print (x, y)'''
+    
 import sys
 
 N = int(sys.stdin.readline().rstrip())
