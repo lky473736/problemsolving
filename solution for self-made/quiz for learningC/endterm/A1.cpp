25c78c8ab7f80c5ffae20b23f040925eb9b739d7
임규연 <lky473736@icloud.com>
2023-12-04 15:35:14 +0900

    Update A1.cpp

--- solution for self-made/quiz for learningC/endterm/A1.cpp
+++ solution for self-made/quiz for learningC/endterm/A1.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main()
@@ -20,5 +21,51 @@ int main()
         }
         
         printf ("==============\n");
+    }
+}*/
+
+#include <stdio.h>
+
+int main()
+{
+    int gugudan[9][9];
+    
+    /*
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            gugudan[i][j] = (i + 1) * (j + 1);
+        }
+    }*/
+    
+    int i, j;
+    
+    i = 0;
+    while (i < 9)
+    {
+        j = 0;
+        while (j < 9)
+        {
+            gugudan[i][j] = (i + 1) * (j + 1);
+            j++;
+        }
+        i++;
+    }
+    
+    i = 0;
+    
+    while (i < 9)
+    {
+        j = 0;
+        
+        while (j < 9)
+        {
+            printf ("%d * %d == %d\n", i + 1, j + 1, gugudan[i][j]);
+            j++;
+        }
+        
+        printf ("===========\n");
+        i++;
     }
 }
