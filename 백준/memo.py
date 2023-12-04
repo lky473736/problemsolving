573655a97bc026ba30703e4448aaeda77a872241
임규연 <lky473736@icloud.com>
2023-12-04 21:00:50 +0900

    time attack

--- 백준/memo.py
+++ 백준/memo.py
@@ -1,53 +1,25 @@
+# hot italy
+
 N, M = map(int, input().split())
-square = []
 
-def findindex(num, arr) :
-    slist = []
-    for i in range (len(arr)) :
-        if num == arr[i] :
-            slist.append (i)
-            
-    return slist
-
+donot = [[] for i in range (N)]
 finder = []
-for i in range (N) : 
-    compo = input()
-    finder.append ([])
-    for x in range (len(compo)) :
-        finder[-1].append (int(compo[x]))
+
+for i in range (M) :
+    com1, com2 = map(int, input().split())
+    donot[com1 - 1].append (com2)
     
-print (finder)
-    
-for j in range (N) : # for j in finder
-    for num in finder[j] :
-        if finder[j].count(num) >= 2 : # >=
-            indexlist = findindex(num, finder[j])
-            print (indexlist)
+for i in range (N) :
+    for j in range (N) : 
+        if j + 1 not in donot[i] and i + 1 not in donot[j] and j + 1 != i + 1 : 
+            for k in range (N) :
+                if j + 1 not in donot[i] and i + 1 not in donot[j] and k + 1 not in donot[i] and k + 1 not in donot[j] and j + 1 not in donot[k] and i + 1 not in donot[k] and k + 1 != j + 1  :
+                    if set([i + 1, j + 1, k + 1]) not in finder and len(set([i + 1, j + 1, k + 1])) == 3 :
+                        finder.append (list([i + 1, j + 1, k + 1]))
 
-            for k in range (j + 1, N) :
-                if finder[k].count(num) == 2 : # >=
-                    guess = findindex(num, finder[k])
-                    print (guess)
-                    
-                    for p in range (len(indexlist)) : # 이웃하지 않아도 체크해야 함 
-                        if p == len(indexlist) - 1:
-                            compo_il = indexlist[-1] - indexlist[0]
-                        else:
-                            compo_il = indexlist[p+1] - indexlist[p]
-                        
-                        for q in range(len(guess)):
-                            if q == len(indexlist) - 1:
-                                compo_gu = guess[-1] - guess[0]
-                            else:
-                                compo_gu = guess[q+1] - guess[q]
 
-                                if (k - j == compo_il and compo_il == compo_gu) :
-                                    square.append ((k - j + 1) ** 2)
+list_of_sets = finder
+unique_sets = [tuple(sorted(set(x))) for x in list_of_sets]
+result = list(set(unique_sets))
 
-print (square)
-
-if square == [] :
-    print (1)
-    
-else :
-    print (max(square))
+print (len(result))
