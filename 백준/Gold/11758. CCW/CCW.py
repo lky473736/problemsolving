75aa4341f731fe9a091d2d2b6ff84b728b02333e
임규연 <lky473736@icloud.com>
2023-12-19 14:23:32 +0900

    [Gold V] Title: CCW, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.py
@@ -0,0 +1,17 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+
+if 1 :
+    # https://coloredrabbit.tistory.com/162
+    
+    comp = (y2 - y1) * (x3 - x2) - (y3 - y2) * (x2 - x1)
+    
+    if comp > 0 :
+        print (-1)
+        
+    elif comp < 0 :
+        print (1)
+        
+    else :
+        print (0)
