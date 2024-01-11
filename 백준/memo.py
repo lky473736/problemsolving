d24143ff717a406e94f744efc144cdb6f8572db9
Gyuyeon Lim <lky473736@icloud.com>
2024-01-11 18:46:00 +0900

    memo for 네 개의 소수, 골드바흐의 추측

--- 백준/memo.py
+++ 백준/memo.py
@@ -1,25 +1,75 @@
-# hot italy
+import sys
+import math 
+import itertools
 
-N, M = map(int, input().split())
-
-donot = [[] for i in range (N)]
-finder = []
-
-for i in range (M) :
-    com1, com2 = map(int, input().split())
-    donot[com1 - 1].append (com2)
+def eratosthenes (M, N) :
+    nums = [0 for i in range (N + 1)]
     
-for i in range (N) :
-    for j in range (N) : 
-        if j + 1 not in donot[i] and i + 1 not in donot[j] and j + 1 != i + 1 : 
-            for k in range (N) :
-                if j + 1 not in donot[i] and i + 1 not in donot[j] and k + 1 not in donot[i] and k + 1 not in donot[j] and j + 1 not in donot[k] and i + 1 not in donot[k] and k + 1 != j + 1  :
-                    if set([i + 1, j + 1, k + 1]) not in finder and len(set([i + 1, j + 1, k + 1])) == 3 :
-                        finder.append (list([i + 1, j + 1, k + 1]))
+    for i in range (2, int(math.sqrt(N) + 1)) : # 0, 1은 소수 아님
+        if nums[i] == 0 :
+            for j in range (i + i, N + 1, i) :
+                nums[j] = 1
+    
+    return nums[1:]
+
+N = int(sys.stdin.readline())
+
+if N == 1 or N == 2 or N == 3 or N == 4 or N == 5 or N == 6 or N == 7 :
+    print(-1)
+    exit()
+
+prime = eratosthenes(1, N)
+prime = [index+1 for index in range(N) if prime[index] == 0]
+prime.pop(0)
+
+combi_prime = itertools.combinations(prime, 4)
+
+for i in combi_prime :
+    if sum(i) == N : 
+        break
+    
+for compo in i : 
+    print(compo, end = ' ')
 
 
-list_of_sets = finder
-unique_sets = [tuple(sorted(set(x))) for x in list_of_sets]
-result = list(set(unique_sets))
 
-print (len(result))
+###################################
+
+
+
+import sys
+import math 
+import itertools
+
+def eratosthenes (M, N) :
+    nums = [0 for i in range (N + 1)]
+    
+    for i in range (2, int(math.sqrt(N) + 1)) : # 0, 1은 소수 아님
+        if nums[i] == 0 :
+            for j in range (i + i, N + 1, i) :
+                nums[j] = 1
+    
+    return nums[1:]
+    
+T = int(sys.stdin.readline())
+
+prime = eratosthenes(1, 10000)
+prime[0] = 1
+
+for i in range (T) :
+    n = int(sys.stdin.readline())
+    
+    nprime = prime[:n].copy()
+    nprime = [index+1 for index in range(n) if nprime[index] == 0]
+
+    combi_repla = itertools.combinations_with_replacement(nprime, 2)
+    
+    list_for_sort = []
+    
+    for j in combi_repla : 
+        if sum(j) == n : 
+            list_for_sort.append([j, abs(j[0]-j[1])])
+            
+    list_for_sort.sort(key = lambda x : x[1])
+    
+    print (list_for_sort[0][0][0], list_for_sort[0][0][1])
