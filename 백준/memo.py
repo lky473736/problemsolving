1118899510bb7204cd4975cf375d2fc8aaa9692f
임규연 <lky473736@icloud.com>
2024-02-20 00:52:58 +0900

    memo for "등비수열의 합 : 23848"

--- 백준/memo.py
+++ 백준/memo.py
@@ -1,44 +1,72 @@
 import sys
-import itertools
-
-N, M = map(int, sys.stdin.readline().split())
-sequence = list(map(int, sys.stdin.readline().split()))
-
-lst = set()
-
-for i in range (M+1) : 
-    for j in itertools.permutations(sequence, N - i) : 
-        for k in itertools.permutations(j, 2) :
-            print (k)
-            if k[0] < k[1] :
-                lst.add (k[1]-k[0])
-                print (lst)
-            
-            print ('-' * 10)
-                
-print (lst)
-...
-
-########
-
-# nested loopstations is baddest in this situation!
-
-import sys
+import math
 
 N = int(sys.stdin.readline())
-A = list(map(int, sys.stdin.readline().split()))
 
-for i in range (N-1) : 
-    key = 0
-    for j in range (i+1, N) : 
-        if A[i] < A[j] : 
-            key = 1
-            break
-    
-    if key == 0 :
-        print (-1)
+if N < 7 :
+    print (-1)
+    exit()
+
+'''while True :
+    if suma > N :
+        a1 = 1
+        r += 1
         
-    else :    
-        print (A[j])
+    arr = [a1, a1*r, a1*r*r]
+    length_arr = 3
+    suma = a1 * (1 + r + r**2)
     
-print (-1)
+    while True :
+        if suma == N :
+            break
+        
+        elif suma > N :
+            a1 += 1
+            break
+            
+        arr.append (a1 * r**(length_arr))
+        suma += a1 * r**(length_arr)
+        length_arr += 1
+        
+    if suma == N : 
+        break'''
+
+def sumation(a1, r, n) :
+    return a1 * (r**n - 1) // (r - 1)
+        
+a1 = 1
+
+while True : 
+    if a1 == N : 
+        print (-1)
+        exit()
+    
+    for r in range (2, int(math.sqrt(N)) + 1) :
+        for counting in range (3, int(math.log(N, r)) + 2) : 
+            if sumation(a1, r, counting) == N : 
+                print (counting)
+                    
+                for i in range (counting) : 
+                    print (a1*r**i, end = ' ')
+                        
+                exit()
+                    
+            elif sumation(a1, r, counting) > N :
+                break
+            
+    a1 += 1
+
+            
+    '''while True :
+        if sumation(a1, r, 3) > N : 
+            break
+        
+        elif sumation(a1, r, counting) == N :
+            break
+        
+        a1 += 1
+        arr.append (a1*r**counting)
+        counting += 1
+        
+    if sumation(a1, r, counting) == N : 
+        break'''
