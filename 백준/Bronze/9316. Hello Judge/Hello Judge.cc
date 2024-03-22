98ff58bb42b1cb53c23c5975c48a334da228cca7
임규연 <lky473736@icloud.com>
2024-03-22 15:06:55 +0900

    [Bronze IV] Title: Hello Judge, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+
+int main() {
+    int N;
+    
+    std::cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        std::cout << "Hello World, Judge " << i+1 << "!" << std::endl;
+    }
+    
+    return 0;
+}
