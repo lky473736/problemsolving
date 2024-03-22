059bed0763488142299aa0497ac31d5188a437b0
임규연 <lky473736@icloud.com>
2024-03-22 20:24:15 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+int main() {
+    // \    /\
+    //  )  ( ')
+    // (  /  )
+    //  \(__)|
+    
+    printf ("\\    /\\\n");
+    printf (" )  ( \')\n");
+    printf ("(  /  )\n");
+    printf (" \\(__)|");
+    
+    return 0;
+}
