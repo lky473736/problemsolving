f3f427ee0169115440c14ca9dfde81462c18c8e6
임규연 <lky473736@icloud.com>
2024-03-23 15:17:12 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
+int main() {
+    int R1, S;
+    scanf ("%d %d", &R1, &S);
+    printf ("%d", 2*S-R1);
+	
+	return 0;
+}
