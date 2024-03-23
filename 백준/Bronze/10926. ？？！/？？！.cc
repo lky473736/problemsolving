53092678b46cb64d86c95e640add267b1eeaab2b
임규연 <lky473736@icloud.com>
2024-03-23 15:07:10 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
+int main() {
+    std::string s;
+    std::cin >> s;
+    
+    std::cout << s << "??!";
+    
+    return 0;
+}
