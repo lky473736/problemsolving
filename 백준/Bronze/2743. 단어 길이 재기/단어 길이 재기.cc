0b1e7a61276374752983b5df176603cacaca38f0
임규연 <lky473736@icloud.com>
2024-03-23 10:29:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <algorithm>
+
+int main() {
+    char string[101];
+    
+    scanf ("%s", string);
+    
+    printf ("%d\n", strlen(string));
+    
+    return 0;
+}
