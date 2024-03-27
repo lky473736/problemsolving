972b31c306464cbfff062c26c9d3bde865a3f798
임규연 <lky473736@icloud.com>
2024-03-27 21:46:31 +0900

    memo for urls

---
+++ 백준/memo.py
@@ -0,0 +1,72 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for it in range (n) :
+    URL = sys.stdin.readline().rstrip()
+    URL += '*'
+    
+    colon = 0
+    slash = 0
+    token_colon = 0
+    token_host = 0
+    
+    protocol = ""
+    host = ""
+    port = ""
+    path = ""
+    
+    i = 0
+    
+    while URL[i] != '*' :
+        if URL[i] == ':' :
+            if colon == 0 : 
+                protocol = URL[:i]
+                
+            elif slash == 2 and colon == 1 : 
+                token_colon = i
+                
+            colon += 1
+            
+        if URL[i] == '/' : 
+            if token_host > 0 or token_colon > 0 :
+                token_host = 0
+                
+            if slash == 1 : 
+                token_host = i+1
+                
+            elif slash == 2 :
+                break
+            
+            slash += 1
+            
+        if URL[i] != '/' and URL[i] != ':' :
+            if token_host > 0 :
+                host += URL[i]
+                
+            elif token_host == 0 : 
+                if token_colon > 0 :
+                    port += URL[i]
+        
+        i += 1
+        
+    print (f"URL #{it+1}")
+    
+    if token_colon != 0 : # 토큰이 있을 때
+        print ("Protocol =", protocol)
+        print ("Host     =", host[:-1 * (i-token_colon)+1])
+        print ("Port     =", URL[token_colon+1:i])
+        print ("Path     =", URL[i+1:-1])
+        
+    else : # 토큰이 없을 때
+        print ("Protocol =", protocol)
+        print ("Host     =", host)
+        print ("Port     = <default>")
+        
+        if URL[i+1:-1] == '' :
+            print ("Path     = <default>")
+            
+        else :
+            print ("Path     =", URL[i+1:-1])
+        
+    print ()
