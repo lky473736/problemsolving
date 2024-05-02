e857c5443293411ec425ad6d1f68fdf6b699fa52
임규연 <lky473736@icloud.com>
2024-05-02 21:46:20 +0900

    [Silver IV] Title: 현대모비스 선풍기 굴리기, Time: 40 ms, Memory: 33240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31534. 현대모비스 선풍기 굴리기/현대모비스 선풍기 굴리기.py
+++ 백준/Silver/31534. 현대모비스 선풍기 굴리기/현대모비스 선풍기 굴리기.py
@@ -3,17 +3,21 @@ import math
 
 a, b, h = map(int, sys.stdin.readline().split())
 
-if a == 0 and a == b : 
+if a == b : 
     print (-1)
     exit()
 
-k1 = math.sqrt(a**2 + (a*h / (b-a))**2)
-k2 = math.sqrt(h**2 + (b-a)**2)
+else :
+    if a < b : 
+        pass
+        
+    else : 
+        a, b = b, a
+        
+    k1 = math.sqrt(a**2 + (a*h / (b-a))**2)
+    k2 = math.sqrt(h**2 + (b-a)**2)
+        
+    rst = math.pi * ((k1 + k2)**2 - k1**2)
+        
 
-rst = math.pi * ((k1 + k2)**2 - k1**2)
-
-if rst != 0 :
-    print (rst)
-    
-else : 
-    print (-1)
+print (rst)
