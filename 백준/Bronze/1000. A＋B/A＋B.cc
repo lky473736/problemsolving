9e888b1e23d105fd39b09e5074fdab80ea1ceeb4
임규연 <lky473736@icloud.com>
2024-06-23 18:28:34 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    
+    int A, B;
+    std::cin >> A >> B;
+    std::cout << A + B;
+    
+    return 0;
+}
