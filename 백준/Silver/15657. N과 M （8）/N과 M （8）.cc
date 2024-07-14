4a5b3804600945d1c668afb2e2759239dff2d752
임규연 <lky473736@icloud.com>
2024-07-15 08:35:27 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> arr;  
+vector<int> nums;
+
+void func(int start, int cnt, int n, int m) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = start; i < n; i++) {
+        arr.push_back(nums[i]);
+        func(i, cnt + 1, n, m);
+        arr.pop_back(); 
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    nums.resize(n); 
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums.begin(), nums.end()); 
+    
+    func(0, 0, n, m); 
+    
+    return 0;
+}
