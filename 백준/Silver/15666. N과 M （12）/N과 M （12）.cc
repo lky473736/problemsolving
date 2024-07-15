a2c38cb929e292fd24c5b1dc0835c4d965766e5a
임규연 <lky473736@icloud.com>
2024-07-15 13:08:52 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int ans[10];
+
+void func(int k, int idx) {
+   // base condition
+   if (k == m) {
+      for (int i = 0; i < m; i++) {
+         cout << ans[i] << " ";
+      }
+      cout << "\n";
+      return;
+   }
+
+   int temp = 0;
+   for (int i = idx; i < n; i++) {
+      if (temp != arr[i]) {
+         ans[k] = arr[i];
+         temp = ans[k];
+         func(k + 1, i);
+      }
+   }
+
+}
+
+int main() {
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+   cin >> n >> m;
+
+   for (int i = 0; i < n; i++) {
+      cin >> arr[i];
+   }
+   sort(arr, arr + n);
+
+   func(0, 0);
+}
