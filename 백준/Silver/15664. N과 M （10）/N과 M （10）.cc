a8a1e6832ed0c393e35d5c7f9d58bca493decb1d
임규연 <lky473736@icloud.com>
2024-07-15 11:21:35 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int nums[10];
+// set<vector<int>> total;
+
+void func(int cnt, int start) {
+    if (cnt == m) {
+        for (int i = 0; i < m; ++i) {
+            cout << arr[i] << ' ';
+        }
+        
+        cout << '\n';
+        return;
+    }
+
+    int prev = 0;
+    for (int i = start; i < n; ++i) {
+        if (prev != nums[i]) {
+            arr[cnt] = nums[i];
+            prev = arr[cnt];
+            func(cnt+1, i+1); 
+            // arr.pop_back();
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    
+    sort(nums, nums + n);
+    
+    func(0, 0);
+    
+    return 0;
+}
