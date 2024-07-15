4879b8651ee927f5e1ffe923bffb42923ae803c8
임규연 <lky473736@icloud.com>
2024-07-15 12:53:01 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -3,40 +3,38 @@ using namespace std;
 
 int n, m;
 int arr[10];
-int nums[10];
-// set<vector<int>> total;
+int ans[10];
 
-void func(int cnt, int start) {
-    if (cnt == m) {
-        for (int i = 0; i < m; i++) {
-            cout << arr[i] << ' ';
-        }
-        
-        cout << '\n';
-        return;
-    }
+void func(int k, int idx) {
+   // base condition
+   if (k == m) {
+      for (int i = 0; i < m; i++) {
+         cout << ans[i] << " ";
+      }
+      cout << "\n";
+      return;
+   }
 
-    int prev = 0;
-    for (int i = start; i < n; i++) {
-        if (prev != nums[i]) {
-            arr[cnt] = nums[i];
-            prev = arr[cnt];
-            func(cnt+1, i+1); 
-            // arr.pop_back();
-        }
-    }
+   int temp = 0;
+   for (int i = idx; i < n; i++) {
+      if (temp != arr[i]) {
+         ans[k] = arr[i];
+         temp = ans[k];
+         func(k + 1, i + 1);
+      }
+   }
 }
 
 int main() {
-    cin >> n >> m;
-    
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-    
-    sort(nums, nums + n);
-    
-    func(0, 0);
-    
-    return 0;
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+   cin >> n >> m;
+
+   for (int i = 0; i < n; i++) {
+      cin >> arr[i];
+   }
+   sort(arr, arr + n);
+
+   func(0,0);
 }
