6c1a3f237d794ea487c5d7d84636f002343fb065
Gyuyeon Lim <lky473736@icloud.com>
2024-09-22 16:15:54 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 168 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Scanner input = new Scanner(System.in); 
+        
+        int A = input.nextInt();
+        int B = input.nextInt();
+        
+        if (A > B) {
+            System.out.println(">");
+        }
+        
+        else if (A < B) {
+            System.out.println("<");
+        }
+        
+        else {
+            System.out.println("==");
+        }
+    }
+}
