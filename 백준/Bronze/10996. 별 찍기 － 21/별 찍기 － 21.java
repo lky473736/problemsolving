2ef9202f71476c907e5492ed2e9c16c87936da66
Gyuyeon Lim <lky473736@icloud.com>
2024-09-29 14:40:36 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 408 ms, Memory: 25116 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Scanner input = new Scanner(System.in); 
+        
+        int n = input.nextInt();
+        
+        for (int cnt = 1; cnt <= n; cnt++) {
+            int p = n;
+            for (int i = 1; i <= 2; i++) {
+                if (i % 2 != 0) {
+                    ; 
+                }
+                
+                else {
+                    System.out.print(' ');
+                }
+                
+                for (int j = 1; j <= p; j++) {
+                    if (j % 2 != 0) {
+                        System.out.print('*');
+                    }
+                        
+                    else {
+                        System.out.print(' ');
+                    }
+                }
+                
+                System.out.println();
+                
+                p -= 1;
+            }
+        }
+    }
+}
