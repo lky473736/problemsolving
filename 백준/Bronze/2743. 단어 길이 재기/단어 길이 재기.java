451dddd33592207b78c207e106dd8a2b6d05042e
Gyuyeon Lim <lky473736@icloud.com>
2024-11-27 18:52:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String word = scan.next();
+        System.out.println(word.length());
+    }
+}
