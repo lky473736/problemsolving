3b5aead4d42bfa98fd5bb79ae0c8afdd255c9d9e
Gyuyeon Lim <lky473736@icloud.com>
2025-01-04 15:24:22 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -1,36 +1,23 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    
-    int arr[4], cnt = 0;
-    
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0);
+
     for (int i = 0; i < 3; i++) {
-        cin >> arr[0] >> arr[1] >> arr[2] >> arr[3];
-        
-        cnt = 0;
-        
+        int cnt_0 = 0; 
         for (int j = 0; j < 4; j++) {
-            if (arr[j] == 1) {
-                cnt++;
-            }
+            int compo;
+            cin >> compo;
+            if (compo == 0) cnt_0++;
         }
-        
-        switch (cnt) {
-            case 0 : cout << "D\n"; break;
-            case 1 : cout << "C\n"; break;
-            case 2 : cout << "B\n"; break;
-            case 3 : cout << "A\n"; break;
-            case 4 : cout << "E\n"; break;
-            
-            default : 
-                break;
-        }
+
+        if (cnt_0 == 0) cout << 'E';
+        if (cnt_0 == 1) cout << 'A';
+        if (cnt_0 == 2) cout << 'B';
+        if (cnt_0 == 3) cout << 'C';
+        if (cnt_0 == 4) cout << 'D';
+        cout << '\n';
     }
-    
     return 0;
 }
