b5d1831cf5c0c1f463922b40628c28756f56e83c
Gyuyeon Lim <lky473736@icloud.com>
2025-01-04 15:31:39 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.cc
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -1,22 +1,18 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    
-    int arr[5], sum = 0;
-    
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int arr[5];
+    int sum = 0;
     for (int i = 0; i < 5; i++) {
         cin >> arr[i];
         sum += arr[i];
-    }    
-    
-    sort(arr, arr+5);
-    
-    cout << sum/5 << '\n' << arr[2];
+    }
+    sort (arr, arr + 5);
+    cout << sum / 5 << '\n';
+    cout << arr[2];
     
     return 0;
 }
