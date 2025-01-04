e586a962db34ba690f100918a39954b93d2ef06a
Gyuyeon Lim <lky473736@icloud.com>
2025-01-04 15:10:13 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,18 +1,13 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    
-    long long arr[3];
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int arr[3];
     cin >> arr[0] >> arr[1] >> arr[2];
-    
-    sort(arr, arr+3);
-    
-    cout << *arr << ' ' << *(arr+1) << ' ' << *(arr+2);
+    sort (arr, arr+3);
+    for (int i = 0; i < 3; i++) cout << arr[i] << ' ';
     
     return 0;
 }
