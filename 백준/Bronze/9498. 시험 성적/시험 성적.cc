6e95013ea4566be1865fd963f9f10aafcbffdff4
Gyuyeon Lim <lky473736@icloud.com>
2025-01-04 15:06:18 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,33 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    
-    int score;
-    cin >> score;
-    
-    if (score >= 90) {
-        cout << "A";
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int A;
+    cin >> A;
+    char c;
+
+    if (A >= 90) {
+        c = 'A';
+    } 
+    else if (A >= 80) {
+        c = 'B';
     }
-    
-    else if (score >= 80) {
-        cout << "B";
+    else if (A >= 70) { 
+        c = 'C';
     }
-    
-    else if (score >= 70) {
-        cout << "C";
+    else if (A >= 60) {
+        c = 'D';
     }
-    
-    else if (score >= 60) {
-        cout << "D";
-    }
-    
     else {
-        cout << "F";
+        c = 'F';
     }
-    
+
+    cout << c;
+
     return 0;
 }
