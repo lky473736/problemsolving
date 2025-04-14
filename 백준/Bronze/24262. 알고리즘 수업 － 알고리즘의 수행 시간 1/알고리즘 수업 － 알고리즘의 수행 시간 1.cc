877ee224e7714fb434074beabc17c500fcf7ecf5
Gyuyeon Lim <lky473736@gmail.com>
2025-04-14 09:07:34 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int MenOfPassion(int *A, int n) {
+    int i = n / 2;
+    return A[i]; 
+}
+
+int main() {
+    int n; cin >> n;
+    cout << 1 << '\n' << '0';
+    return 0;
+}
