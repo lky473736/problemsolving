e0d97471b04e1a2d2b683a6c44d7486307d262f0
Gyuyeon Lim <lky473736@gmail.com>
2025-04-16 01:56:18 +0900

    beering

---
+++ class/mergeSort_beer.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int S[] = {1, 9, 2, 3, 4};
+int N = 5; 
+int call_num_1 = 0;
+int call_num_2 = 0;
+
+void merge (int h, int m, int *U, int *V, int *S) {
+    // i for U, j for V, k for S 
+    int i = 0, j = 0, k = 0;
+    
+    while (i < h && i < m) {
+        if (U[i] < V[j]) {
+            S[k] = U[i];
+            i++;
+        } else {
+            S[k] = V[j];
+            j++;
+        }
+        k++;
+    }
+    
+    if (i > h) {
+        while (j < m) { S[k++] = V[j++]; }
+    } else {
+        while (i < h) { S[k++] = U[i++]; }
+    }
+    
+    cout << "call merge : " << ++call_num_2 << endl;
+    cout << "U : ";
+    for (int i = 0; i < h; i++) { cout << U[i] << ' '; }
+    cout << endl;
+    cout << "V : ";
+    for (int i = 0; i < m; i++) { cout << V[i] << ' '; }
+    cout << endl;
+    cout << "S : ";
+    for (int i = 0; i < N; i++) { cout << S[i] << ' '; }
+    cout << endl;
+    cout << endl;
+}
+
+void mergeSort(int n, int *arr) {
+    if (n > 1) {
+        int h = floor(n/2);
+        int m = n-h;
+        int *U = new int[h];
+        int *V = new int[m];
+        
+        // divide
+        for (int i = 0; i < h; i++) { U[i] = S[i]; }
+        for (int i = 0; i < m; i++) { V[i] = S[i+h]; }
+        
+        cout << "call mergeSort : " << ++call_num_1 << endl;
+        cout << "U : ";
+        for (int i = 0; i < h; i++) { cout << U[i] << ' '; }
+        cout << endl;
+        cout << "V : ";
+        for (int i = 0; i < m; i++) { cout << V[i] << ' '; }
+        cout << endl;
+        cout << endl;
+        
+        // conquer
+        mergeSort(h, U);
+        mergeSort(m, V);
+        
+        // combine
+        merge (h, m, U, V, S);
+    }
+}
+
+int main()
+{
+    mergeSort(5, S);
+    for (int i = 0; i < 5; i++) { cout << S[i] << ' ';}
+}
