e2cab58c0a90ff5da3ed0cb990fe6eb8ace4ddd5
Gyuyeon Lim <lky473736@gmail.com>
2025-04-16 11:15:44 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24314. 알고리즘 수업 － 점근적 표기 2/알고리즘 수업 － 점근적 표기 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a1, a0; cin >> a1 >> a0;
+    int c; cin >> c;
+    int n0; cin >> n0;
+    
+    if (a1 * n0 + a0 >= c * n0 and a1 >= c) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    
+    return 0;
+}
